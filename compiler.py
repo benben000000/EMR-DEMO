77474bea45422e068b21708a0477c3ddef5198e5
benben000000 <bmgarcia0121@gmail.com>
2026-08-24 14:40:35 +0800

    chore: Clean up build scratch scripts

--- compiler.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# compiler.py
-# Writes serve_demo.py, api/index.py, public/, and root html files
-
-with open('/Users/kloudtech/Project/compiler.py', 'w', encoding='utf-8') as f:
-    f.write('''import os
-print("Compiling full HMIS suite...")
-''')
--- create_full_hmis_server.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# We will write the complete serve_demo.py directly using Python file writer
-print("Generating complete 33-module EMR suite with Departmental RBAC and Search...")
-
-output_path = '/Users/kloudtech/Project/serve_demo.py'
